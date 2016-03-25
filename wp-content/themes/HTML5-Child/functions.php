88eac0bc356fcdd71ecf1337c32aed39db3bebb1
Lindsay <lpeters322@gmail.com>
2016-03-25 16:38:23 -0700

    Plugin Installs
    Yoast SEO and Admin Menu Editor

--- wp-content/themes/HTML5-Child/functions.php
+++ wp-content/themes/HTML5-Child/functions.php
@@ -13,7 +13,21 @@ function theme_enqueue_styles() {
     wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array($parent_style));
 }
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
-
 /*  Add your own functions below this line.
     ======================================== */ 
-
+    function html5blank_styles_make_child_active()
+    {
+    wp_deregister_style('html5blank');
+    }
+    add_action('wp_enqueue_scripts', 'html5blank_styles_make_child_active', 100); 
+    function html5blank_styles_child()
+    {
+    wp_register_style('html5blank-child', get_stylesheet_directory_uri() . '/style.css', array(), '1.0', 'all');
+    wp_enqueue_style('html5blank-child'); 
+    }
+    add_action('wp_enqueue_scripts', 'html5blank_styles_child'); 
+	add_action( 'after_setup_theme', 'wpt_setup' );
+    if ( ! function_exists( 'wpt_setup' ) ):
+        function wpt_setup() {  
+            register_nav_menu( 'primary', __( 'Primary navigation', 'wptuts' ) );
+        } endif;
