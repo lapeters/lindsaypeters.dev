da4ebe825d8db08ee0edceff8906ea38337d4385
Lindsay <lpeters322@gmail.com>
2016-05-06 10:21:26 -0700

    Template Page for Photography

--- wp-content/themes/planum-child/page-photography.php
+++ wp-content/themes/planum-child/page-photography.php
@@ -7,34 +7,35 @@
 
 get_header(); ?>
 
-
 	<div id="content" class="site-content clearfix" style="background: #<?php echo get_theme_mod( 'background_color','FFF' ); ?> url(<?php echo get_theme_mod( 'background_image' ); ?>);">
 
 	<?php if ( has_post_thumbnail() ) {
 		the_post_thumbnail( 'planum-featured' );
 	} ?>
 
-		<div class="content-full" role="main">
+	<?php if ( has_excerpt() ) : ?>
+		<div class="topexcerpt"><?php the_excerpt(); ?></div>
+	<?php endif; //has_excerpt() ?>	
 
+		<div class="content-full" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<header class="entry-header">
-				<h1 class="entry-title"><?php the_title(); ?></h1>
-				</header><!-- .entry-header -->
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<header class="entry-header">
+					<h1 class="entry-title"><?php the_title(); ?></h1>
+					</header><!-- .entry-header -->
 
-			<div class="entry-content">
+				<div class="entry-content">
 				<?php the_content(); ?>
-			<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'planum' ),
-				'after'  => '</div>',
-			) );
-		?>
-
-	</div><!-- .entry-content -->
-</article><!-- #post-<?php the_ID(); ?> -->
+				<?php
+					wp_link_pages( array(
+					'before' => '<div class="page-links">' . __( 'Pages:', 'planum' ),
+					'after'  => '</div>',
+					) );
+					?>
+				</div><!-- .entry-content -->
+			</article><!-- #post-<?php the_ID(); ?> -->
 
 			<?php endwhile; // end of the loop. ?>
 
